f701b05e12b7f4f8af4010b61ce2c595b2b9d0dd
nyxx-star <aizirocontato@gmail.com>
2025-03-29 01:03:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,14 @@ export default {
 
     // Roteamento de arquivos
     if (path === "/inject.js") {
-      const file = await env.ASSETS.fetch("https://raw.githubusercontent.com/SEU_USUARIO/SEU_REPO/main/inject.js");
+      const file = await env.ASSETS.fetch("https://raw.githubusercontent.com/nyxx-star/status-everywhere/main/inject.js");
       return new Response(file.body, { headers: { "Content-Type": "application/javascript" } });
     }
     else if (path === "/plugin.json") {
-      const file = await env.ASSETS.fetch("https://raw.githubusercontent.com/SEU_USUARIO/SEU_REPO/main/plugin.json");
+      const file = await env.ASSETS.fetch("https://raw.githubusercontent.com/nyxx-star/status-everywhere/main/plugin.json");
       return new Response(file.body, { headers: { "Content-Type": "application/json" } });
     }
     else if (path === "/styles.css") {
-      const file = await env.ASSETS.fetch("https://raw.githubusercontent.com/SEU_USUARIO/SEU_REPO/main/styles.css");
+      const file = await env.ASSETS.fetch("https://raw.githubusercontent.com/nyxx-star/status-everywhere/main/styles.css");
       return new Response(file.body, { headers: { "Content-Type": "text/css" } });
     }
-
-    // Página inicial (opcional)
-    return new Response(`
-      <h1>Plugin Pyoncord</h1>
-      <p>Arquivos disponíveis:</p>
-      <ul>
-        <li><a href="/inject.js">inject.js</a></li>
-        <li><a href="/plugin.json">plugin.json</a></li>
-        <li><a href="/styles.css">styles.css</a></li>
-      </ul>
-    `, { headers: { "Content-Type": "text/html" } });
-  }
-};
